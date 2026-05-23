6d3cfb1f2eb82064fba66cc9049202bfff63625f
Julius Polar <julius.polar@gmail.com>
2026-05-23 16:28:32 +0200

    cleanup

--- packages/domos/src/routes/+page.svelte
+++ packages/domos/src/routes/+page.svelte
@@ -1,23 +1,5 @@
 <script lang="ts">
   import Card from "$lib/card/card.svelte";
-  import MarkdownEditor from "$lib/markdown-editor/markdown-editor.svelte";
-
-  let markdownContent = `# Hello Markdown
-\`\`\`ts
-console.log("Hello World")
-\`\`\`
-
-## This is a markdown editor built with CodeMirror in Svelte.
-
-This is a (hyperlink)[https://example.com] and this is **bold text**.
-`;
-  const handleMarkdownChange = (newContent: string) => {
-    markdownContent = newContent;
-  };
 </script>
 
-<Card>
-  <MarkdownEditor
-    value={markdownContent}
-    onChange={handleMarkdownChange} />
-</Card>
+<Card>domos</Card>
